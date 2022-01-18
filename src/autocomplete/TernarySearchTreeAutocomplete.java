2714973a2ea6f2923ec47738d0619ec2d84c2306
ainsleylaiuw <ainslai@uw.edu>
2022-01-17 16:38:51 -0800

    worked on TST, forgot to push yesterday

--- src/autocomplete/TernarySearchTreeAutocomplete.java
+++ src/autocomplete/TernarySearchTreeAutocomplete.java
@@ -13,7 +13,7 @@ public class TernarySearchTreeAutocomplete implements Autocomplete {
      * The overall root of the tree: the first character of the first autocompletion term added to this tree.
      */
     private Node overallRoot;
-
+    private int n; //size
     /**
      * Constructs an empty instance.
      */
@@ -21,10 +21,91 @@ public class TernarySearchTreeAutocomplete implements Autocomplete {
         overallRoot = null;
     }
 
+
+    /**
+     * Returns the value associated with the given key.
+     * @param key the key
+     * @return the value associated with the given key if the key is in the symbol table
+     *     and {@code null} if the key is not in the symbol table
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Boolean get(String key) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls get() with null argument");
+        }
+        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+        Node x = get(overallRoot, key, 0);
+        if (x == null) return null;
+        return x.isTerm;
+    }
+
+    // return subtrie corresponding to given key
+    private Node get(Node x, String key, int d) {
+        if (x == null) return null;
+        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+        char c = key.charAt(d);
+        if      (c < x.data)              return get(x.left,  key, d);
+        else if (c > x.data)              return get(x.right, key, d);
+        else if (d < key.length() - 1) return get(x.mid,   key, d+1);
+        else                           return x;
+    }
+
+    /**
+     * Does this symbol table contain the given key?
+     * @param key the key
+     * @return {@code true} if this symbol table contains {@code key} and
+     *     {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public boolean contains(String key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to contains() is null");
+        }
+        return get(key) != null;
+    }
+
+    /**
+     * Inserts the key-value pair into the symbol table, overwriting the old value
+     * with the new value if the key is already in the symbol table.
+     * If the value is {@code null}, this effectively deletes the key from the symbol table.
+     * @param key the key
+     * @param val the value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void put(String key, Boolean val) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls put() with null key");
+        }
+        if (!contains(key)) n++;
+        else if(val == null) n--;       // delete existing key
+        overallRoot = put(overallRoot, key, val, 0);
+        //
+        if (!contains(key)) {
+            overallRoot = put(overallRoot, key, val, 0);
+        } else if (val == null) {
+
+        }
+    }
+
+    private Node put(Node x, String key, Boolean val, int d) {
+        char c = key.charAt(d);
+        if (x == null) {
+            x = new Node(c);
+        }
+        if      (c < x.data)               x.left  = put(x.left,  key, val, d);
+        else if (c > x.data)               x.right = put(x.right, key, val, d);
+        else if (d < key.length() - 1)  x.mid   = put(x.mid,   key, val, d+1);
+        else                            x.isTerm   = val;
+        return x;
+    }
+
     @Override
+    // deciding which key to start with
+    // what does value mean
     public void addAll(Collection<? extends CharSequence> terms) {
-        // TODO: Replace with your code
-        throw new UnsupportedOperationException("Not implemented yet");
+        for (CharSequence term : terms) {
+            put(term.toString(), get(term.toString()));
+        }
     }
 
     @Override
@@ -52,3 +133,5 @@ public class TernarySearchTreeAutocomplete implements Autocomplete {
         }
     }
 }
+
+
